4fd6105e4a88b6ad3b3c0aa662d60e02c0f57e86
Ahadi <ahadic044@gmail.com>
2024-09-16 18:33:26 +0200

    [ADD] Forbidden errorhandler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauth() -> None:
     """ An endpoint to test unauthorized error handler """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def unauth() -> None:
+    """ An endpoint to test forbidden error handler """
+    abort(403)
